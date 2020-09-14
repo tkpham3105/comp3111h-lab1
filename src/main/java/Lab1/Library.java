baacedaf61aeb8aa8f970e0c93c47ddafa786090
trung <trung@tkpham>
2020-09-14 21:19:18 +0700

    Initial project setup

---
+++ src/main/java/Lab1/Library.java
@@ -0,0 +1,8 @@
+public class Library {
+	public static void main(String arg[]) {
+		System.out.println("When there is a fire, commit and push.");
+	}
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
